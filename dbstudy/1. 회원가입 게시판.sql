8c7adf7a70a9e3dcdd526b35683dd7164400527f
lesngn82 <lesngjn82@gmail.com>
2022-11-27 18:16:05 +0900

    20221127

--- dbstudy/1. 회원가입 게시판.sql
+++ dbstudy/1. 회원가입 게시판.sql
@@ -2,8 +2,6 @@
 DROP SEQUENCE USERS_SEQ;
 CREATE SEQUENCE USERS_SEQ NOCACHE;
 
-
-
 DROP TABLE USERS;
 CREATE TABLE USERS (
 	USER_NO	                NUMBER	            NOT NULL,
@@ -33,3 +31,25 @@ CREATE TABLE USERS (
 ALTER TABLE USERS ADD CONSTRAINT PK_USERS PRIMARY KEY (
 	USER_NO
 );
+
+-- ID : 유니크키 
+-- * 한 테이블에 PK는 오직 하나만 줄 수 있으며, PK는 UK의 일종이기 때문에 PK가 존재하는 상황에 UK를 만들 수 없다
+ALTER TABLE USERS ADD CONSTRAINT UK_USERS UNIQUE(ID);
+
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디1', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디2', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디3', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디4', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디5', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디6', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디7', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디8', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디9', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디10', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디11', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디12', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디13', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디14', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+INSERT INTO USERS VALUES (USERS_SEQ.NEXTVAL, '아이디15', '비밀번호', '이름', '1', '이메일', '010', '1996', '82', 'CO', '주소1' , '주소2', '상세주소', '추가주소', 0, NULL, SYSDATE, SYSDATE, SYSDATE, NULL, NULL, 0);
+
+COMMIT;
