93bda68fddf7bdd1d662a7df49872e79ad707638
lesngjn82 <lesngjn82@gmail.com>
2022-12-09 17:53:17 +0900

    20221209

--- bootstudy/practice/src/main/java/com/gdu/practice/controller/Controller.java
+++ bootstudy/practice/src/main/java/com/gdu/practice/controller/Controller.java
@@ -1,6 +1,13 @@
 package com.gdu.practice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "index";
+	}
 
 }
